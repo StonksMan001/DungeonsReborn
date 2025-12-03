efe43c7526b68a1275b2b19734ee772985ced9e9
StonksMan001 <toth.abel.2007@gmail.com>
2025-12-03 18:27:11 +0100

    v0.0.5 - GameRules are now prefixed with "dr_"

--- src/main/java/net/stonksman01/dungeons_reborn/registries/MCD_GameRules.java
+++ src/main/java/net/stonksman01/dungeons_reborn/registries/MCD_GameRules.java
@@ -4,9 +4,10 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
 import net.stonksman01.dungeons_reborn.DungeonsReborn;
+import net.stonksman01.dungeons_reborn._included_libs.skycore.SkyCore;
 
 public class MCD_GameRules {
-    public static final GameRules.Key<GameRules.BooleanRule> POP_FLOWERS_ALWAYS_HIDE = GameRuleRegistry.register("popFlowersAlwaysHide", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(false));
+    public static final GameRules.Key<GameRules.BooleanRule> POP_FLOWERS_ALWAYS_HIDE = SkyCore.BuiltinRegistries.registerGameRule("popFlowersAlwaysHide", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(false));
     public static void register() {
         DungeonsReborn.LOGGER.info("[DungeonsReborn] Registering GameRules");
     }
