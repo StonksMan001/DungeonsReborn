7cea37e5bad4d9efc75fa4264242c0387ae800e5
StonksMan001 <toth.abel.2007@gmail.com>
2025-12-06 18:38:16 +0100

    v0.0.7 - Fixed critical bug on servers

--- src/main/java/net/stonksman01/dungeons_reborn/DungeonsReborn.java
+++ src/main/java/net/stonksman01/dungeons_reborn/DungeonsReborn.java
@@ -24,7 +24,6 @@ public class DungeonsReborn implements ModInitializer {
 		MCD_Items.register();
 		MCD_ItemTags.register();
 		MCD_LootTableModifiers.register();
-		MCD_Models.register();
 		MCD_Sounds.register();
 		MCD_ToolMaterials.register();
 	}
