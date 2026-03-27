514073fae7d8cef305a9858adeddd4f51d6d52f5
StonksMan001 <toth.abel.2007@gmail.com>
2026-03-27 02:00:59 +0100

    v0.1.0 - preparing branch for 26.1 full rewrite (fresh fabric project)

--- remappedSrc/net/stonksman01/dungeons_reborn/_included_libs/skycore/items/SC_CrossbowItem.java
+++
@@ -1,9 +0,0 @@
-package net.stonksman01.dungeons_reborn._included_libs.skycore.items;
-
-import net.minecraft.item.CrossbowItem;
-
-public class SC_CrossbowItem extends CrossbowItem {
-    public SC_CrossbowItem(net.minecraft.item.Item.Settings settings) {
-        super(settings);
-    }
-}
